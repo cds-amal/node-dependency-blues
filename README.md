462403c156fc5daa7768320a4baf1feee0b965cc
cds-amal <cds.sudama@gmail.com>
2023-01-12 10:49:13 -0500

    initial

---
+++ README.md
@@ -0,0 +1,14 @@
+## Awesome commandline tool
+
+`npm start <arg>...`
+
+**Example:**
+
+```console
+$ npm start AB cD ef
+
+> run-to-completion@1.0.0 start
+> node command.js AB cD ef
+
+[ 'AB', 'CD', 'EF' ]
+```
---
+++ command.js
@@ -0,0 +1,11 @@
+function processInput(args) {
+  // Validate input: it should never contain a "/"
+  if (args.some(arg => /\//.test(arg))) {
+    throw new Error("Slash sucks");
+  }
+  return args.map(s => s.toUpperCase());
+}
+
+args = processInput(process.argv.slice(2));
+console.log(args);
+
